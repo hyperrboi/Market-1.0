73323f0c528055b18cc484e7392d3238fcdd5087
Paulo Renato <paulorenato107@icloud.com>
2026-07-31 13:29:53 -0300

    Adicionando as funções de cadastrar e mostrar o catalógo ao sistema

--- src/modelos/Produto.java
+++ src/modelos/Produto.java
@@ -2,6 +2,32 @@ package modelos;
 
 public class Produto {
     private String nome;
-    private int id;
+    private int id, unidades;
     private double precoCaixa, precoUnitario;
+
+    public Produto(String nome, int id, int unidades, double precoCaixa, double margemLucro) {
+        this.nome = nome;
+        this.id = id;
+        this.precoCaixa = precoCaixa;
+        this.unidades = unidades;
+        this.precoUnitario = calcularPreco(unidades, precoCaixa, margemLucro);
+    }
+
+    private double calcularPreco(int unidades, double precoCaixa, double margemLucro) {
+        return (this.precoCaixa / this.unidades ) /  ( (100 - margemLucro) / 100) ;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: %s/ NOME: %s/ UNIDADES: %d/ PRECOCAIXA: R$%.2f/ PRECOUNITARIO: R$%.2f".formatted(this.id,
+                this.nome, this.unidades, this.precoCaixa, this.precoUnitario);
+    }
 }
--- src/servicos/Mercado.java
+++ src/servicos/Mercado.java
@@ -2,7 +2,28 @@ package servicos;
 
 import modelos.Produto;
 
+import java.util.ArrayList;
+
 public class Mercado {
     private Catalogo catalogo;
     private Inventario inventario;
+    private double margemLucro = 15;
+    private static int id = 1;
+
+    public Mercado() {
+        catalogo = new Catalogo();
+        inventario = new Inventario();
+    }
+
+    public boolean cadastrarProduto(String nome, int unidades, double precoCaixa) {
+        if (nome.isBlank() || (unidades <= 0) || (precoCaixa <= 0)) {
+            return false;
+        }
+        Produto p = new Produto(nome, (this.id++), unidades, precoCaixa, this.margemLucro);
+        return catalogo.adicionarAoCatalogo(p);
+    }
+
+    public Catalogo getCatalogo() {
+        return catalogo;
+    }
 }
